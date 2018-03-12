d2550bdd1f4b962a3530b8ffe13748ddcf8c5334
John-Paul Stanford <John-Paul.Stanford@arm.com>
2018-03-12 11:45:27 +0000

    Actuall fixed the shutdown

--- shutdown.sh
+++ shutdown.sh
@@ -2,7 +2,4 @@
 
 set -e
 
-DBUSER=$(cat /tmp/pgbouncer/etc/userlist.txt | cut -d' ' -f 1 | sed 's/\"//g')
-DBPASSWD=$(cat /tmp/pgbouncer/etc/userlist.txt | cut -d' ' -f 2 | sed 's/\"//g')
-
-PGPASSWORD="$DBPASSWD" psql --port=6432 --username=$DBUSER --host /tmp/pgbouncer/run/ pgbouncer --command='SHUTDOWN;'
+su - postgres -c "psql --port=6432 --username=pgbouncer --host /tmp/pgbouncer/run/ --no-password pgbouncer --command='SHUTDOWN'"
